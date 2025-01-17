45331b08188a1cbbe630907c5a0452713c01f123
livi514 <livia5banyai@gmail.com>
2025-01-17 20:42:35 +0000

    Create scan.py

---
+++ scan.py
@@ -0,0 +1,5 @@
+'''
+SCAN algorithm. This algorithm has the lift move in one direction and only
+changes direction when it reaches the bottom or top floors. It serves all requests
+while travelling in that direction
+'''
