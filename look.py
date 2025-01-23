b11fbea980cec3d62adefbfe3e7c0bb7cf9bc28d
livi514 <livia5banyai@gmail.com>
2025-01-23 23:18:11 +0000

    new improved version of the look algorithm

--- look.py
+++ look.py
@@ -3,54 +3,16 @@ LOOK algorithm. Like SCAN, but the direction can be changed at any point if no
 more requests exist in the current direction
 '''
 
-#number of floors - any positive integer
-number_of_floors = 10
+#ground floor - floor number 0
 
-#direction of travel - "up" or "down"
-direction_of_travel = "up"
-
+top_floor_number = 10 #value can be any positive integer
+number_of_floors = top_floor_number + 1 #top floor number + the ground floor (floor 0)
+direction_of_travel = "up" #can be "up" or "down"
 current_floor = 0
 
-TOP_FLOOR = number_of_floors
-BOTTOM_FLOOR = 0
+#example requests - we will need to change the way that requests are processed for the real-time version of the algorithm 
+request_list = [6, 2, 8, 7, 5]
 
-#will be updated to true later on
-top_floor_reached = False
-
-#lift will start on the ground floor?
-bottom_floor_reached = True
-
-#for now, I'm not implementing priorities 
-
-#just making up some requests so far - for real time I think we have to let users input them somehow?
-request_list = [8, 7, 2, 5, 6]  
-
-#here, a lift journey will be either the lift travelling downwards or upwards, when the lift changes direction, this will begin a new "journey"
+#function to handle one lift journey - here, a lift journey will be the lift either travelling upwards or downwards
 def lift_journey():
-  global current_floor, direction_of_travel, request_list
-  #for now I haven't implemented different priorities
-  #sorting the list in ascending or descending order based on the direction of travel 
-  #e.g. if we're going up we'll start with the request with the lowest number
-  if direction_of_travel == "up":
-    #sorting the list in ascending order
-    request_list.sort()
-  elif direction_of_travel == "down":
-    request_list.sort(reverse=True)
-  floors = list(range(number_of_floors + 1))
-  for i in floors:
-    current_floor = i
-    print("Current floor:" , current_floor)
-    if i in request_list:
-      request_list.remove(i)
-      print("Request list:", request_list)
-    if (current_floor == TOP_FLOOR) or (len(request_list == 0)): #this means we can turn the lift around
-      #switching the lift direction
-      if direction_of_travel == "up":
-        direction_of_travel == "down"
-      elif direction_of_travel == "down":
-        direction_of_travel == "up"
-      print("Changing direction to", direction_of_travel)
-      break
-
-lift_journey()
-
+  #I've planned out the algorithm I just need to type it up in python 
