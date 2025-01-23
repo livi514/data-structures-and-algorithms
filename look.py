2ad58dadd3ada41254a64c4020ee8ba52fce9dcf
livi514 <livia5banyai@gmail.com>
2025-01-23 17:20:25 +0000

    Update look.py

--- look.py
+++ look.py
@@ -21,4 +21,15 @@ bottom_floor_reached = true
 #for now, I'm not implementing priorities 
 
 #just making up some requests so far - for real time I think we have to let users input them somehow?
-request_list = [2, 3, 5, 7, 9, 10]
+request_list = [10, 7, 2, 5, 6]  
+
+#here, a lift journey will be either the lift travelling downwards or upwards, when the lift changes direction, this will begin a new "journey"
+def lift_journey():
+  #for now I haven't implemented different priorities
+  #sorting the list in ascending or descending order based on the direction of travel 
+  #e.g. if we're going up we'll start with the request with the lowest number
+  if direction_of_travel == "up":
+    #sorting the list in ascending order
+    request_list.sort()
+
+  
