cf6084b89b958ee9766cfed09b43aa7550a99fb7
livi514 <livia5banyai@gmail.com>
2025-01-24 11:59:29 +0000

    debugging look.py (there are still errors)

--- look.py
+++ look.py
@@ -3,12 +3,14 @@ LOOK algorithm. Like SCAN, but the direction can be changed at any point if no
 more requests exist in the current direction
 '''
 
+#CURRENTLY DEBUGGING
+
 #ground floor - floor number 0
 
 top_floor_number = 10 #value can be any positive integer
 number_of_floors = top_floor_number + 1 #top floor number + the ground floor (floor 0)
 direction_of_travel = "up" #can be "up" or "down"
-current_floor = 0
+current_floor = 5
 
 #example requests - we will need to change the way that requests are processed for the real-time version of the algorithm 
 request_list = [6, 2, 8, 7, 5]
@@ -24,15 +26,18 @@ while len(request_list) != 0:
     else: #direction_of_travel == "up"
       direction_of_travel == "down"
     print("Changing direction to", direction_of_travel)
+  
+  '''
   for floor in requests_sorted:
     current_floor = floor
     print("Current floor", floor)
     request_list.remove(floor)
     print("Remaining requests", request_list)
-  if direction_of_travel == "up":
-    if not any (requests > current_floor for requests in request_list):
-      direction_of_travel == "down"
-  else:
-    if not any (requests < current_floor for requests in request_list):
-      direction_of_travel == "up"
-  print("Changing dierection to", direction_of_travel)
+    if direction_of_travel == "up":
+      if not any (requests > current_floor for requests in request_list):
+        direction_of_travel == "down"
+    else:
+      if not any (requests < current_floor for requests in request_list):
+        direction_of_travel == "up"
+    print("Changing direction to", direction_of_travel)
+  '''
