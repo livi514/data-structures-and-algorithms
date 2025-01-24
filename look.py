069ba13fd2c7c68702e3de0287ca3507f9eedbf6
livi514 <livia5banyai@gmail.com>
2025-01-24 19:32:56 +0000

    fixing some errors + other small changes to look.py

--- look.py
+++ look.py
@@ -3,8 +3,6 @@ LOOK algorithm. Like SCAN, but the direction can be changed at any point if no
 more requests exist in the current direction
 '''
 
-#ground_floor - floor number 0
-
 top_floor_number = 10 #value can be any positive integer
 number_of_floors = top_floor_number + 1 #top floor number + the ground_floor (floor 0)
 direction_of_travel = "up" #can be "up" or "down"
@@ -13,8 +11,6 @@ current_floor = 10
 #example requests - we will need to change the way that requests are processed for the real-time version of the algorithm
 request_list = []
 
-#this is my old code which didn't work-------------------------------------------------------------------
-#I think I was able to get some bugs. Should work fine - Kim
 def lift():#Made this into a function so it calls everything within it
     global direction_of_travel, current_floor
 
@@ -24,20 +20,17 @@ def lift():#Made this into a function so it calls everything within it
     #Step 1: Collecting the user floor input
     #Right now it just takes in one input. Will need to fix that
     while True:
-        try: #added exception handling - Livi
-            user_input = int(input(f"Enter a floor(0-{top_floor_number}) or type 'exit' to stop program: ")) # changed this to int(input()) - Livi
-        except TypeError:
-            print("Input must be an integer")
-        except: #for errors other than TypeError
-            print("Invalid input.")
-        if user_input.lower == "exit": #This is an extra feature if you want the lift to stop before all the floors are done. Still woring on the listing part.
+        user_input = input(f"Enter a floor(0-{top_floor_number}) or type 'exit' to stop program: ").strip().lower() 
+        if user_input == "exit": #This is an extra feature if you want the lift to stop before all the floors are done. Still woring on the listing part.
             print(f"Stopping Program. Remaining requests: {request_list}")
             break
-        if (user_input.isnumeric()) and (user_input > 0): #changed this to isnumeric() and >0 as the input can be any positive integer not just digits - Livi
-            floor_requests = int(user_input)
-            if 0 <= floor_requests < top_floor_number: #This checks if the floor request is within the ground floor(0) and top floor(10)
-                request_list.append(floor_requests)
-                print(f"Added floor requests: {floor_requests}")
+        if (user_input.isnumeric()): #changed this to isnumeric() as the input can be any positive integer not just digits - Livi
+            floor_request = int(user_input)
+
+            #changed this to <= top_floor number rather than < top_floor number - Livi
+            if 0 <= floor_request <= top_floor_number: #This checks if the floor request is within the ground floor(0) and top floor(10)
+                request_list.append(floor_request)
+                print(f"Added floor requests: {floor_request}")
             else: print(f"Invalid input. Please enter a valid floor number (0-{top_floor_number}) or type 'exit' to stop program: ")
         else:
           print(f'Please enter a valid floor number (0-{top_floor_number}) or type "exit" to stop program:')
