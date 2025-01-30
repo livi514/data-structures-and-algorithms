cd79f085dcccdd49fa324af4c628fd6e3a8d28de
livi514 <livia5banyai@gmail.com>
2025-01-30 17:44:47 +0000

    Update look v3.py

--- look v3.py
+++ look v3.py
@@ -7,7 +7,7 @@ import time
 #processing the input file 
 def process_input_file():
   input_file = open("input.txt")
-  file.readline() #reads the first line but doesn't do anything with it, as the first line is just a description of the second file 
-  floors_and_capacity = file.readline() #gets the second line of the file
-  
-  
+  input_file.readline() #reads the first line but doesn't do anything with it, as the first line is just a description of the second file 
+  floors_and_capacity = input_file.readline().split(",") #gets the second line of the file
+  print(floors_and_capacity)
+
