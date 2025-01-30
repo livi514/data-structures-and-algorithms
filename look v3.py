686dee0ab58dd90c2c72ed03b6c6071b0d0b274d
livi514 <livia5banyai@gmail.com>
2025-01-30 18:14:49 +0000

    Update look v3.py

--- look v3.py
+++ look v3.py
@@ -8,12 +8,11 @@ import time
 def process_input_file():
   input_file = open("input.txt")
   input_file.readline() #reads the first line but doesn't do anything with it, as the first line is just a description of the second file 
-  floors_and_capacity = input_file.readline().split(",") #gets the second line of the file
-  floors = floors_and_capacity[0].strip()
-  capacity = floors_and_capacity[1].strip()
-  print(floors)
-  print(capacity)
+  floors_and_capacity = input_file.readline().split(",").strip() #gets the second line of the file
+  floors = int(floors_and_capacity[0].strip())
+  capacity = int(floors_and_capacity[1].strip())
   input_file.readline() #reading the third line
+  floor_requests = {}
   for line in input_file():
     floor_and_requests = line.split(":")
     
