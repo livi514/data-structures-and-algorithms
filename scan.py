a842e7a5c5ec4ae486aa99ffdfa319337ca5122c
thalia <thaliachamp05@gmail.com>
2025-01-30 17:33:21 +0000

    Update scan.py

--- scan.py
+++ scan.py
@@ -3,3 +3,10 @@ SCAN algorithm. This algorithm has the lift move in one direction and only
 changes direction when it reaches the bottom or top floors. It serves all requests
 while travelling in that direction
 '''
+def ElevatorScan(requests, CurrentFloor, direction="up"):
+  # takes three arguments. requests is a list of floor requests
+  while requests:
+    if direction == "up":
+      requests.sort()
+    else: 
+      requests.sort(reverse=True)
