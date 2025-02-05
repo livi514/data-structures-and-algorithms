4a39cd78b22046705ef01d8e2a794de3b5b884ed
livi514 <livia5banyai@gmail.com>
2025-02-05 17:40:28 +0000

    adding excepttion handling to look v5

--- look v5 real time requests.py
+++ look v5 real time requests.py
@@ -5,27 +5,32 @@ def input_file():
     building_info = {}
     document = input("Please enter a lift file name: ")
     document = document.strip()
+    #editing the code so that it can handle the input whether the user includes ".txt" at the end or not - Livi
     if document.endswith(".txt"):
         file = document
     else:
         file = document + ".txt"
-    with open(file) as f:
-        for line in f:
-            line = line.strip()
-            #print(line)
-            if line.startswith("#") or not line:
-                continue # This will skip the comments and any empty line -Kim
+    #added exception handling - Livi
+    try:
+        with open(file) as f:
+            for line in f:
+                line = line.strip()
+                #print(line)
+                if line.startswith("#") or not line:
+                    continue # This will skip the comments and any empty line -Kim
 
-            if "," in line and ":" not in line:
-                num_floors, capacity = map(int, line.split(","))
-                building_info["num_floors"] = num_floors
-                building_info["capacity"] = capacity
-                continue
+                if "," in line and ":" not in line:
+                    num_floors, capacity = map(int, line.split(","))
+                    building_info["num_floors"] = num_floors
+                    building_info["capacity"] = capacity
+                    continue
 
-            floor, requests = line.split(":")
-            floor = int(floor.strip())
-            requests = [int(r) for r in requests.split(",") if r.strip()] #Converting the values into integers
-            floor_requests[floor] = requests #Putting the values into the dictionary
+                floor, requests = line.split(":")
+                floor = int(floor.strip())
+                requests = [int(r) for r in requests.split(",") if r.strip()] #Converting the values into integers
+                floor_requests[floor] = requests #Putting the values into the dictionary
+    except:
+        print("File cannot be found, please try again.")
     print("Building information:")
     print(f"Number of floors: {building_info['num_floors']}")
     print(f"Lift capacity: {building_info['capacity']} \n")
